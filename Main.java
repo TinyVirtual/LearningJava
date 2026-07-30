bad4666cb2db21608c7db3ace1c625d760f3dc38
Tiny <tiny.the.virtual@gmail.com>
2026-07-30 13:30:37 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.util.Date;
+
 public class Main {
+
     public static void main(String[] args){
         System.out.println("Hello world!");
         // Great, this is so fucking simple, but so fucking hard since i still know nothing
         System.out.println("Your arguments are \"" + String.join("\", \"", args) + "\" with a length of " + String.valueOf(args.length) );
+
+        for(int i = 0; i < 10; i+=1){
+            System.out.println(String.format("%.4f",Math.random()) + " generated at "+ new Date().toString());
+        }
+        // ok, still bullshit, but here is something to start with
     }
 }
